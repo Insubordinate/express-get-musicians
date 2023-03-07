7754796b9cbc882697d5ebda9ce918fbd7c9d0aa
Axl Cruz <acruz8344@gmail.com>
2023-03-07 09:36:09 -0500

    Added endpoints

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const {sequelize} = require("./db")
 
 const port = 3000;
 
+app.use(express.json())
+app.use(express.urlencoded())
+
 //TODO
 
 app.listen(port, () => {
@@ -19,4 +22,29 @@ app.get('/musicians',async(req,res)=>{
 
 app.get('/musicians/:id',async(req,res)=>{
     res.json(await Musician.findByPk(req.params.id))
+})
+
+
+
+// Handle POST
+
+
+app.post('/musicians',async(req,res)=>{
+    await Musician.create(req.body)
+    res.json(await Musician.findAll())
+})
+
+
+
+// Handle PUT
+app.put('/musicians/:id',async(req,res)=>{
+    await Musician.update(req.body,{where:{id:req.params.id}})
+    res.json(await Musician.findAll())
+})
+
+//HANDLE DELETE
+
+app.delete('/musicians/:id',async(req,res)=>{
+    await Musician.destroy({where:{id:req.params.id}})
+    res.json(await Musician.findAll())
 })
